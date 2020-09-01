217ed899683b3071379525866e3174841fb28874
Анатолий Литвинов <program.virus.login@gmail.com>
2020-09-02 01:46:46 +0300

    Реализовано подключение к базе данных MySQL

---
+++ AuthApp/Migrations/20200901222046_Initial Create.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace AuthApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Login = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ AuthApp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string Login { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string Password { get; set; }
+    }
+}
